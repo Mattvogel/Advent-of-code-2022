eb40e30cfa0764c0fcfdb7f8e4a2b02db596f682
Matthew Vogel <matthew.scott.vogel@gmail.com>
2022-12-02 22:17:40 +1000

    day2 part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -17,34 +17,44 @@ fn main() {
     buf_reader.read_to_string(&mut contents).unwrap();
 
     let mut score_win: i32 = 0;
+    let mut score_fudged: i32 = 0;
     for line in contents.lines() {
         let strat: Vec<&str> = line.split_whitespace().collect();
         match strat[0] {
             "A" => { //Rock
-                if strat[1] == "X" { //Rock
-                    score_win = score_win + draw + rock
-                } else if strat[1] == "Y" { //Paper
-                    score_win = score_win + win + paper
-                } else { //Scissors
-                    score_win = score_win  + scissors
+                if strat[1] == "X" { //Rock / Lose
+                    score_win = score_win + draw + rock;
+                    score_fudged = score_fudged + scissors;
+                } else if strat[1] == "Y" { //Paper / Draw
+                    score_win = score_win + win + paper;
+                    score_fudged = score_fudged + rock + draw
+                } else { //Scissors / Win
+                    score_win = score_win + scissors;
+                    score_fudged = score_fudged + paper + win;
                 }
             }
             "B" => { //Paper
                 if strat[1] == "X" {
-                    score_win = score_win + rock
+                    score_win = score_win + rock;
+                    score_fudged = score_fudged + rock;
                 } else if strat[1] == "Y" {
-                    score_win = score_win + draw + paper
+                    score_win = score_win + draw + paper;
+                    score_fudged = score_fudged + draw + paper;
                 } else {
-                    score_win = score_win + win + scissors
+                    score_win = score_win + win + scissors;
+                    score_fudged = score_fudged + win + scissors;
                 }
             }
             "C" => { //Scissors
                 if strat[1] == "X" {
-                    score_win = score_win + win + rock
+                    score_win = score_win + win + rock;
+                    score_fudged = score_fudged + paper;
                 } else if strat[1] == "Y" {
-                    score_win = score_win + paper
+                    score_win = score_win + paper;
+                    score_fudged = score_fudged + scissors + draw;
                 } else {
-                    score_win = score_win + draw + scissors
+                    score_win = score_win + draw + scissors;
+                    score_fudged = score_fudged + win + rock;
                 }
             }
             _ => {
@@ -53,5 +63,6 @@ fn main() {
         }
 
     }
-    println!("{}", score_win);
+    println!("Winning score: {}", score_win);
+    println!("Fudged score: {}", score_fudged);
 }
